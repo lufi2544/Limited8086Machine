1f393251f5f9e2cb83fb7cccf067358c283ec51b
Juanes Rayo <lufi2544@gmail.com>
2024-09-20 10:07:24 +0200

    Started adding course decoder

---
+++ build/sim86_memory.cpp
@@ -0,0 +1,22 @@
+﻿#include "sim86_memory.h"
+
+u32 LoadMemoryFromFile(char* FileName, memory* Memory, u32 AtOffset)
+{
+    u32 Result = 0;
+
+    if(AtOffset < ArrayCount(Memory->Bytes))
+    {
+        FILE *File = {};
+        if(fopen_s(&File, FileName, "rb") == 0)
+        {
+            Result = fread(Memory->Bytes + AtOffset, 1, ArrayCount(Memory->Bytes) - AtOffset, File);
+            fclose(File);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "ERROR: Unable to open the file %s\n", FileName);
+    }
+
+    return Result;
+}
---
+++ build/sim86_memory.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "sim8086_types.h"
+
+struct memory
+{
+    u8 Bytes[1024 * 1024];
+};
+
+
+static u32 LoadMemoryFromFile(char* FileName, memory* Memory, u32 AtOffset);
